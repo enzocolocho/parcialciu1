d347e682555d04894604d624b4810dd83f555d3e
enzocolocho <enzotomas.colocho@estudiantes.unahur.edu.ar>
2023-05-12 16:37:25 -0300

    casi finalizado

--- index.js
+++ index.js
@@ -4,3 +4,18 @@ var cambiarModo = function dark_mode(){
     document.documentElement.classList.toggle('dark-mode');
 }
 boton.addEventListener("click", cambiarModo);
+
+
+/* FORMULARIO */
+const $form = document.getElementById('#form')
+const $buttonMailto = document.querySelector('#hiddenahref')
+
+$form.addEventListener('submit', handleSubmit)
+
+function handleSubmit(event) {
+    event.preventDefault()
+    const form = new FormData(this)
+    console.log(form.get('name'));
+    $buttonMailto.setAttribute('href', `mailto:enzo.colocho.99@gmail.com?subject=nombre ${form.get('name')}  correo ${form.get('email')}&body=${form.get('comentario')}`)
+    $buttonMailto.click()
+}
