1ccc8ffe8f768382f9db53e9fdd05927e1491c67
enzocolocho <enzotomas.colocho@estudiantes.unahur.edu.ar>
2023-05-12 21:48:23 -0300

    .

--- index.js
+++ index.js
@@ -6,17 +6,27 @@ var cambiarModo = function dark_mode(){
 boton.addEventListener("click", cambiarModo);
 
 
-/* FORMULARIO 
+// FORMULARIO 
 const $form = document.getElementById('#form')
-const $buttonMailto = document.querySelector('#hiddenahref')
+//const $buttonMailto = document.querySelector('#hiddenahref')
 
 $form.addEventListener('submit', handleSubmit)
 
-function handleSubmit(event) {
+async function handleSubmit(event) {
     event.preventDefault()
     const form = new FormData(this)
-    console.log(form.get('name'));
-    $buttonMailto.setAttribute('href', `mailto:enzo.colocho.99@gmail.com?subject=nombre ${form.get('name')}  correo ${form.get('email')}&body=${form.get('comentario')}`)
-    $buttonMailto.click()
+    const response = await fetch(this.action, {
+        method: this.method,
+        body: form,
+        headers: {
+            'Accept': 'aplication/json'
+        }
+    })
+    if (response.ok){
+        this.reset()
+        alert('Gracias por contactarme')
+    }
+
+    //$buttonMailto.setAttribute('href', `mailto:enzo.colocho.99@gmail.com?subject=nombre ${form.get('name')}  correo ${form.get('email')}&body=${form.get('comentario')}`)
+    //$buttonMailto.click()
 }
-*/
